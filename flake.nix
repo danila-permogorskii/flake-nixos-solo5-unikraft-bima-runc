07e3431ed1bdc215830f64cf47fef2297db5f44d
danila <sample@mail.com>
2025-06-05 22:12:54 +0000

    Adjusted flake.nix for solo5

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Minimal unikernel development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -10,6 +10,24 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        solo5 = pkgs.stdenv.mkDerivation rec {
+	  pname = "solo5";
+	  version = "0.9.1";
+          
+
+	  src = pkgs.fetchFromGitHub {
+	    owner = "Solo5";
+	    repo = "solo5";
+	    rev = "v${version}";
+	    sha256 = "...";
+
+	    nativeBuildInputs = [pkgs.pkg-config];
+	    buildInputs = [pkgs.libseccomp];
+	    configurePhase = "./configure.sh";
+	    buildPhase = "make";
+	    InstallPhase = "cp solo5-spt $out/bin/";
+	  };
+	};
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
@@ -18,7 +36,8 @@
             gnumake
             pkg-config
             git
-            
+	    libseccomp # reqired for the Solo5
+	    solo5            
             # Container tools
             docker
           ];
